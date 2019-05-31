10e4bd16ee577c850d1997a6fbedf74c76343dda
Abul Kalam Azad Shaik <abulkalamazad.shaik@fastretailing.com>
2019-05-31 13:34:54 +0900

    Updates

---
+++ precache-manifest.6f4b3bd76460c18241f2ac9194d2cb66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "661636d5083e7c06717e058ffcc0ded4",
+    "url": "/react-counter/index.html"
+  },
+  {
+    "revision": "a4f10b9e2fa117dadfb7",
+    "url": "/react-counter/static/css/main.2e56f9eb.chunk.css"
+  },
+  {
+    "revision": "407ab141deba78abe319",
+    "url": "/react-counter/static/js/2.d042e1ad.chunk.js"
+  },
+  {
+    "revision": "a4f10b9e2fa117dadfb7",
+    "url": "/react-counter/static/js/main.6cb0ac75.chunk.js"
+  },
+  {
+    "revision": "2c73fdf03faab0d3c96b",
+    "url": "/react-counter/static/js/runtime~main.35208266.js"
+  }
+]);
